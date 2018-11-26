7fab98be50cbe270eeba1e6a8f9456fc51737b93
Ouroborosi <wa104c1200332@gmail.com>
2018-11-26 18:16:05 +0800

    created mcq component

---
+++ src/components/MCQ.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+
+  <section class="mcq">
+    <h1>mcq Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'mcq',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .mcq {
+
+  }
+</style>
