4b6ce5b8b0f2743cbae24736f0fd33956793382c
Ouroborosi <wa104c1200332@gmail.com>
2018-11-29 03:39:22 +0800

    import vueRx

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* import vue-rx */
+import VueRx from 'vue-rx'
+
+Vue.use(VueRx)
+
 /* import vue material components*/
 import {MdContent, MdButton, MdCard} from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
